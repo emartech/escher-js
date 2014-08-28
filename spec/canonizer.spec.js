f7e3eb3170756c3c8402ea473b55271135b8ffa1
ttoth <ttoth.t11@gmail.com>
2014-08-28 17:13:17 +0200

    [SUITEDEV-2114] Canonizer

---
+++ spec/canonizer.spec.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var Canonizer = require('../lib/canonizer');
+var http = require('https');
+var fs = require('fs');
+var _ = require('underscore')._;
+
+function readTestFile(testCase, extension) {
+    return fs.readFileSync('spec/aws4_testsuite/' + testCase + '.' + extension, {encoding: 'utf-8'});
+}
+function getMethod(requestLines) {
+    return requestLines[0].split(' ')[0];
+}
+function getUri(requestLines) {
+    return requestLines[0].split(' ')[1];
+}
+function getHeaders(requestLines) {
+    return requestLines.slice(1, -2).reduce(function (acc, headerLine) {
+        var header = headerLine.split(':');
+        acc[header[0]] = header[1];
+        return acc;
+    }, {});
+}
+function getBody(requestLines) {
+    return requestLines[requestLines.length - 1];
+}
+function getHost(headers) {
+    return headers[_.keys(headers).filter(function (key) {
+        return key.toLowerCase() == 'host';
+    })[0]];
+}
+
+describe('Canonizer', function () {
+    describe('canonizeRequest', function () {
+        it('should be an existing method', function () {
+
+            var requestLines = readTestFile('get-vanilla', 'req').split("\n");
+            var body = getBody(requestLines);
+
+            var options = {
+                method: getMethod(requestLines),
+                host: getHost(getHeaders(requestLines)),
+                uri: getUri(requestLines),
+                headers: getHeaders(requestLines)
+            };
+
+            var request = http.request(options);
+
+            var canonizedRequest = new Canonizer().canonizeRequest(request, body);
+        });
+    });
+});
