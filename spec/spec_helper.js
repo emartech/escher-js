397a350d94d9e48508dce091307fd6bc145aa843
Andras Barthazi <andras.barthazi@prezi.com>
2014-08-30 06:47:46 +0200

    [SUITEDEV-2114] fix merge problems

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -14,6 +14,16 @@ function readTestFile(testCase, extension) {
     return fs.readFileSync('spec/aws4_testsuite/' + testCase + '.' + extension, {encoding: 'utf-8'});
 }
 
+function bin2hex(s) {
+    var i, l, o = '', n;
+    s += '';
+    for (i = 0, l = s.length; i < l; i++) {
+        n = s.charCodeAt(i).toString(16);
+        o += n.length < 2 ? '0' + n : n;
+    }
+    return o;
+}
+
 var AWSTestFileParser = function(testFileContent) {
 
     var requestLines = testFileContent.split(/\r\n|\n|\r/);
@@ -21,19 +31,23 @@ var AWSTestFileParser = function(testFileContent) {
     function getMethod() {
         return requestLines[0].split(' ')[0];
     }
+
     function getUri() {
         return requestLines[0].split(' ')[1];
     }
+
     function getHeaders() {
         return requestLines.slice(1, -2).reduce(function (acc, headerLine) {
             var header = headerLine.match(/([^:]*):(.*)/);
-            acc[header[1]] = header[2];
+            acc.push([header[1], header[2]]);
             return acc;
-        }, {});
+        }, []);
     }
+
     function getBody() {
         return requestLines[requestLines.length - 1];
     }
+
     function getHost(headers) {
         return lookupHeader(headers, 'host');
     }
@@ -63,5 +77,6 @@ var AWSTestFileParser = function(testFileContent) {
 module.exports = {
     using: using,
     readTestFile: readTestFile,
-    AWSTestFileParser: AWSTestFileParser
+    AWSTestFileParser: AWSTestFileParser,
+    bin2hex: bin2hex
 };
