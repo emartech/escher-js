81f80fcbc13e9abd2b72fbdc56bf9e4c281735d0
csaba.simon <csaba.simon@emarsys.com>
2014-09-01 13:52:19 +0200

    [SUITEDEV-2114] Order of headers

--- spec/escher.spec.js
+++ spec/escher.spec.js
@@ -38,33 +38,59 @@ describe('Escher', function () {
     });
 
     describe('validateRequest', function () {
-        it('should validate request using auth header', function () {
-            var goodAuthHeader =
-                    'AWS4-HMAC-SHA256 ' +
-                    'Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, ' +
-                    'SignedHeaders=date;host, ' +
-                    'Signature=b27ccfbfa7df52a200ff74193ca6e32d4b48b8856fab7ebf1c595d0670a7e470';
-            var headers = [
-                ['Date', 'Mon, 09 Sep 2011 23:36:00 GMT'],
-                ['Host', 'host.foo.com'],
-                ['Authorization', goodAuthHeader]
-            ];
-            var keyDB = function (accessKey) {
-                var keys = {'AKIDEXAMPLE': 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY'};
-                return keys[accessKey];
-            };
-            var escherConfig =   {
-                authHeaderName:  'Authorization',
-                dateHeaderName:  'Date',
-                algoPrefix:      'AWS4',
+        var goodDate = 'Mon, 09 Sep 2011 23:36:00 GMT';
+        var goodAuthHeader =
+            'AWS4-HMAC-SHA256 ' +
+                'Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, ' +
+                'SignedHeaders=date;host, ' +
+                'Signature=b27ccfbfa7df52a200ff74193ca6e32d4b48b8856fab7ebf1c595d0670a7e470';
+
+        function configWithDate(date) {
+            return {
+                authHeaderName: 'Authorization',
+                dateHeaderName: 'Date',
+                algoPrefix: 'AWS4',
                 credentialScope: 'us-east-1/host/aws4_request',
-                date:            new Date('Mon, 09 Sep 2011 23:40:00 GMT')
+                date: new Date(date)
             };
-            var requestOptions = {
+        }
+
+        function requestOptionsWithHeaders(headers) {
+            return {
                 method: 'GET',
                 host: 'host.foo.com',
                 uri: '/',
                 headers: headers
+            };
+        }
+
+        it('should validate request using auth header', function () {
+            var headers = [
+                ['Date', goodDate],
+                ['Host', 'host.foo.com'],
+                ['Authorization', goodAuthHeader]
+            ];
+            var escherConfig = configWithDate(goodDate);
+            var requestOptions = requestOptionsWithHeaders(headers);
+            var keyDB = function (accessKey) {
+                var keys = {'AKIDEXAMPLE': 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY'};
+                return keys[accessKey];
+            };
+
+            expect(new Escher(escherConfig).validateRequest(requestOptions, '', keyDB)).toBeTruthy();
+        });
+
+        it('should not depend on the order of headers', function () {
+            var headers = [
+                ['Host', 'host.foo.com'],
+                ['Date', goodDate],
+                ['Authorization', goodAuthHeader]
+            ];
+            var escherConfig = configWithDate(goodDate);
+            var requestOptions = requestOptionsWithHeaders(headers);
+            var keyDB = function (accessKey) {
+                var keys = {'AKIDEXAMPLE': 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY'};
+                return keys[accessKey];
             };
 
             expect(new Escher(escherConfig).validateRequest(requestOptions, '', keyDB)).toBeTruthy();
