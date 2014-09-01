b511e2bd5cfa4aedbe107e01c13e9bd92afaeee5
csaba.simon <csaba.simon@emarsys.com>
2014-09-01 11:56:25 +0200

    [SUITEDEV-2114] validate request

--- spec/escher.spec.js
+++ spec/escher.spec.js
@@ -10,4 +10,38 @@ describe('Escher', function () {
 
         });
     });
+
+    describe('validateRequest', function () {
+        it('should validate request using auth header', function () {
+            var goodAuthHeader =
+                    'AWS4-HMAC-SHA256 ' +
+                    'Credential=AKIDEXAMPLE/20110909/us-east-1/host/aws4_request, ' +
+                    'SignedHeaders=date;host, ' +
+                    'Signature=b27ccfbfa7df52a200ff74193ca6e32d4b48b8856fab7ebf1c595d0670a7e470';
+            var headers = [
+                ['Date', 'Mon, 09 Sep 2011 23:36:00 GMT'],
+                ['Host', 'host.foo.com'],
+                ['Authorization', goodAuthHeader]
+            ];
+            var keyDB = function (accessKey) {
+                var keys = {'AKIDEXAMPLE': 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY'};
+                return keys[accessKey];
+            };
+            var escherConfig =   {
+                authHeaderName:  'Authorization',
+                dateHeaderName:  'Date',
+                algoPrefix:      'AWS4',
+                credentialScope: 'us-east-1/host/aws4_request',
+                date:            new Date('Mon, 09 Sep 2011 23:40:00 GMT')
+            };
+            var requestOptions = {
+                method: 'GET',
+                host: 'host.foo.com',
+                uri: '/',
+                headers: headers
+            };
+
+            expect(new Escher(escherConfig).validateRequest(requestOptions, '', keyDB)).toBeTruthy();
+        });
+    });
 });
