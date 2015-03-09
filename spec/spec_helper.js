b66d7c61090ff2f9337def689fb37f3a836ec8e2
Andras Barthazi <andras@barthazi.hu>
2015-03-09 23:03:12 +0100

    using JSON test files

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -29,53 +29,13 @@ function bin2hex(s) {
 }
 
 function getTest(testSuite, testFile) {
-    var test = new TestFileParser(readTestFile(testSuite, testFile, 'req'));
-    test.config = testConfig.getConfigForSuite(testSuite);
-    test.config.date = test.date;
-    test.expected = {
-        "request": new TestFileParser(readTestFile(testSuite, testFile, 'sreq')).request,
-        "canonicalizedRequest": readTestFile(testSuite, testFile, 'creq'),
-        "stringToSign": readTestFile(testSuite, testFile, 'sts'),
-        "authHeader": readTestFile(testSuite, testFile, 'authz')
-    };
-    delete test.date;
-    delete test.request.date;
-    delete test.expected.request.date;
-    // writeTestFile(testSuite, testFile, 'json', JSON.stringify(test, null, 4));
+    var fileName = 'spec/' + testSuite + '_testsuite/' + testFile + '.json';
+    var test = JSON.parse(fs.readFileSync(fileName, {encoding: 'utf-8'}));
+    if (test.config.date) {
+        test.config.date = new Date(test.config.date);
+    }
     return test;
 }
-
-function readTestFile(testSuite, testCase, extension) {
-    var fileName = 'spec/' + testSuite + '_testsuite/' + testCase + '.' + extension;
-    return fs.readFileSync(fileName, {encoding: 'utf-8'});
-}
-
-function writeTestFile(testSuite, testCase, extension, data) {
-    var fileName = 'spec/' + testSuite + '_testsuite/' + testCase + '.' + extension;
-    return fs.writeFileSync(fileName, data, {encoding: 'utf-8'});
-}
-
-var TestFileParser = function(testFileContent) {
-    var requestLines = testFileContent.split(/\r\n|\r|\n/);
-
-    var headersArray = [],
-        headersMap = {};
-
-    requestLines.slice(2, -2).forEach(function (headerLine) {
-        var header = headerLine.match(/([^:]*):(.*)/);
-        headersArray.push([header[1], header[2].trim()]);
-        headersMap[header[1].toLowerCase()] = header[2].trim();
-    });
-
-    this.headersToSign = Object.keys(headersMap);
-    this.date = new Date(headersMap['date']);
-    this.request = {
-        "method": requestLines[0],
-        "url": requestLines[1],
-        "headers": headersArray,
-        "body": requestLines[requestLines.length - 1]
-    };
-};
 
 function createKeyDb(keyDBHash) {
     return function (accessKeyId) {
