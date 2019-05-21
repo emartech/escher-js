9bb265fe6f1bc1193eeaa2c3dc2ff87b01f6ce1a
Balazs Prokics <probalazs@nyenyere.eu>
2019-05-21 18:31:32 +0200

    refactor(escher-test-cases): run canonicalizer tests in tape

---
+++ spec/canonicalizer.spec-tape.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const tape = require('tape');
+const Canonicalizer = require('../lib/canonicalizer');
+
+module.exports = { runCanonicalizerTests };
+
+function runCanonicalizerTests(testCases) {
+  testCases.signrequest.forEach(runCanonicalizeRequestTape);
+}
+
+function runCanonicalizeRequestTape({ test, group, file }) {
+  if (test.expected.canonicalizedRequest) {
+    tape(`[${group}] ${file} | Canonicalizer #canonicalizeRequest`, t => {
+      const canonicalizedRequest = new Canonicalizer('SHA256').canonicalizeRequest(
+        test.request,
+        test.request.body,
+        test.headersToSign,
+      );
+      t.equal(canonicalizedRequest, test.expected.canonicalizedRequest);
+      t.end();
+    });
+  }
+}
--- spec/tape-tests.js
+++ spec/tape-tests.js
@@ -4,9 +4,8 @@ const { getTestCases } = require('./get-test-cases');
 const { runAuthHelperTests } = require('./auth-helper.spec-tape');
 const { runEscherTests } = require('./escher-test-cases.spec-tape');
 const { runSignerTests } = require('./signer.spec-tape');
+const { runCanonicalizerTests } = require('./canonicalizer.spec-tape');
 
-getTestCases('escher-test-cases').then(testCases => {
-  runEscherTests(testCases);
-  runAuthHelperTests(testCases);
-  runSignerTests(testCases);
-});
+getTestCases('escher-test-cases').then(testCases =>
+  [runEscherTests, runAuthHelperTests, runSignerTests, runCanonicalizerTests].forEach(runTests => runTests(testCases)),
+);
