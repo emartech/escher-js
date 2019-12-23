d521a569550452102b5a180cec25e7ddecd4d682
Balazs Prokics <probalazs@nyenyere.eu>
2019-12-23 11:44:45 +0100

    refactor(sign-request): get authorization header

--- lib/sign-request/lib/get-authorization-header/get-authorization-header.ts
+++ lib/sign-request/lib/get-authorization-header/get-authorization-header.ts
@@ -1,6 +1,8 @@
 import { RequestHeader, EscherConfig, ValidRequest, RequestBody } from '../../../interface';
-import { toLower } from 'ramda';
-const AuthHelper = require('../../../authhelper');
+import { toLower, pipe, map, uniq, join, comparator, lt, sort } from 'ramda';
+import Signer = require('../../../signer');
+import { convertToAwsShortDate } from '../convert-to-aws-short-date';
+import { getNormalizedHeaderName } from '../get-normalized-header-name';
 
 export type GetAuthorizationHeader = (
   config: EscherConfig,
@@ -12,6 +14,51 @@ export type GetAuthorizationHeader = (
 
 export const getAuthorizationHeader: GetAuthorizationHeader = (config, date, request, body, headersToSign) => {
   const headerName = toLower(config.authHeaderName);
-  const headerValue = new AuthHelper(config, date).generateHeader(request, body, headersToSign);
+  const headerValue = getHeaderValue(config, date, request, body, headersToSign);
   return [headerName, headerValue];
 };
+
+function getHeaderValue(
+  config: EscherConfig,
+  date: Date,
+  request: ValidRequest,
+  body: RequestBody,
+  headersToSign: string[],
+): string {
+  const authorizationAlgo = getAuthorizationAlgo(config);
+  const credential = getCredential(config, date);
+  const signedHeaders = getSignedHeaders(headersToSign);
+  const signature = getSignature(config, date, request, body, headersToSign);
+  return `${authorizationAlgo} Credential=${credential}, SignedHeaders=${signedHeaders}, Signature=${signature}`;
+}
+
+function getAuthorizationAlgo({ algoPrefix, hashAlgo }: EscherConfig): string {
+  return `${algoPrefix}-HMAC-${hashAlgo}`;
+}
+
+function getCredential({ accessKeyId, credentialScope }: EscherConfig, date: Date): string {
+  return `${accessKeyId}/${convertToAwsShortDate(date)}/${credentialScope}`;
+}
+
+function getSignedHeaders(headersToSign: string[]): string {
+  return pipe(
+    map<string, string>(getNormalizedHeaderName),
+    uniq as any,
+    sort(comparator(lt)),
+    join(';'),
+  )(headersToSign);
+}
+
+function getSignature(
+  config: EscherConfig,
+  date: Date,
+  request: ValidRequest,
+  body: RequestBody,
+  headersToSign: string[],
+): string {
+  const signer = new Signer(config, date);
+  return signer.calculateSignature(
+    signer.getStringToSign(request, body, headersToSign),
+    signer.calculateSigningKey(),
+  ) as any;
+}
