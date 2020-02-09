a705164e1474ac10aa951c1c02784afc1905eb45
Balazs Prokics <balazs.prokics@emarsys.com>
2020-02-09 14:02:32 +0100

    test(lib): get credential test created

--- src/lib/get-credential/get-credential.ts
+++ src/lib/get-credential/get-credential.ts
@@ -1,7 +1,11 @@
-import { EscherConfig } from '../../interface';
 import { convertToAwsShortDate } from '../convert-to-aws-short-date';
 
-export type GetCredential = (config: EscherConfig, date: Date) => string;
+export type CredentialConfig = {
+  accessKeyId: string;
+  credentialScope: string;
+};
+
+export type GetCredential = (config: CredentialConfig, date: Date) => string;
 
 export const getCredential: GetCredential = (config, date) =>
   `${config.accessKeyId}/${convertToAwsShortDate(date)}/${config.credentialScope}`;
