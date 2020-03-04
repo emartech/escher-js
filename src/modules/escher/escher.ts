7ba1639edc4a773aa4725ec97a1a28d9ff207def
probalazs <probalazs@nyenyere.eu>
2020-03-04 22:20:49 +0100

    feat(escher): create deprecated switcher

--- src/modules/escher/escher.ts
+++ src/modules/escher/escher.ts
@@ -1,36 +1,58 @@
-import { EscherConfig, Request } from '../../interface';
+import { EscherConfig, Request, ValidRequest, RequestBody } from '../../interface';
 import { getEscherConfig, checkPartialEscherConfig } from './lib';
 import { validateMandatorySignedHeaders } from '../validate-mandatory-signed-headers';
 import { validateRequest } from '../validate-request';
 import { signRequest } from '../sign-request';
 import { presignUrl } from '../presign-url';
 import { authenticate } from '../authenticate';
+const DeprecatedEscher = require('../../deprecated/deprecated-escher');
 
 export class Escher {
   private _config: EscherConfig;
 
-  constructor(partialConfig?: any) {
+  private _useDeprecated: boolean = false;
+
+  constructor(partialConfig?: Partial<EscherConfig>) {
     checkPartialEscherConfig(partialConfig);
     this._config = getEscherConfig(partialConfig);
   }
 
+  public useDeprecated(useDeprecated: boolean): void {
+    this._useDeprecated = useDeprecated;
+  }
+
   preSignUrl(url: string, expiration: number): string {
+    if (this._useDeprecated) {
+      return DeprecatedEscher.create(this._config).preSignUrl(url, expiration);
+    }
     return presignUrl(this._config, url, expiration, new Date());
   }
 
-  signRequest(requestOptions: any, body: any, headersToSign: any): any {
+  signRequest(requestOptions: ValidRequest, body: RequestBody, headersToSign: string[]): any {
+    if (this._useDeprecated) {
+      return DeprecatedEscher.create(this._config).signRequest(requestOptions, body, headersToSign);
+    }
     return signRequest(this._config, requestOptions, body, headersToSign);
   }
 
-  authenticate(request: any, keyDB: any, mandatorySignedHeaders: any): any {
+  authenticate(request: ValidRequest, keyDB: Function, mandatorySignedHeaders: string[]): any {
+    if (this._useDeprecated) {
+      return DeprecatedEscher.create(this._config).authenticate(request, keyDB, mandatorySignedHeaders);
+    }
     return authenticate(this._config, request, keyDB, mandatorySignedHeaders);
   }
 
-  validateRequest(request: Request, body?: any): void {
+  validateRequest(request: Request, body?: RequestBody): void {
+    if (this._useDeprecated) {
+      DeprecatedEscher.create(this._config).validateRequest(request, body);
+    }
     validateRequest(request, body);
   }
 
-  validateMandatorySignedHeaders(headers?: any): void {
+  validateMandatorySignedHeaders(headers?: string[]): void {
+    if (this._useDeprecated) {
+      DeprecatedEscher.create(this._config).validateMandatorySignedHeaders(headers);
+    }
     validateMandatorySignedHeaders(headers);
   }
 
