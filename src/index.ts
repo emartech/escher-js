66ac779bb3570f063148bb6051d4250743d464f7
probalazs <probalazs@nyenyere.eu>
2020-03-04 22:25:18 +0100

    feat(app): export modules from package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { Escher } from './modules/escher';
 
 export default Escher;
+
+export * from './modules/authenticate';
+export * from './modules/presign-url';
+export * from './modules/sign-request';
+export * from './modules/validate-mandatory-signed-headers';
+export * from './modules/validate-request';
