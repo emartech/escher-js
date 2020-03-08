74f4296af487d93f4bb7723ce84e2a14bf0d1030
probalazs <probalazs@nyenyere.eu>
2020-03-08 18:57:30 +0100

    build(app): export types

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,21 @@ export * from './modules/presign-url';
 export * from './modules/sign-request';
 export * from './modules/validate-mandatory-signed-headers';
 export * from './modules/validate-request';
+
+export {
+  Authenticate,
+  AuthenticateConfig,
+  EscherRequest,
+  PresignUrl,
+  PresignUrlConfig,
+  SignRequest,
+  SignRequestConfg,
+  EscherRequestBody,
+} from './interface';
+
+export {
+  createAuthenticateConfig,
+  createEscherRequest,
+  createPresignUrlConfig,
+  createSignRequestConfg,
+} from './factory';
